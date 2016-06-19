491b8fd760515a01c42a34b232f74fcf8e5a9542
Chris Murray <chris.murray.az@gmail.com>
2016-06-19 13:22:27 -0700

    Tutorial 2 finished

--- src/Swing/Main.java
+++ src/Swing/Main.java
@@ -1,12 +1,14 @@
 package Swing;
 
+//	 Based off of CaveOfProgramming.com YouTube playlist
+
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		JFrame frame = new JFrame("Hello World Swing!");
+		MainFrame frame = new MainFrame("Hello World Swing!");
 		frame.setSize(500,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
---
+++ src/Swing/MainFrame.java
@@ -0,0 +1,42 @@
+package Swing;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class MainFrame extends JFrame
+{
+	public MainFrame(String title)
+	{
+		super(title);
+		
+		//	Set layout manager
+		setLayout(new BorderLayout());
+		
+		//	Create Swing components
+		JTextArea textArea = new JTextArea();
+		JButton button = new JButton("Click Me!");
+		
+		//	Add Swing components to content pane
+		Container c = getContentPane();
+		
+		c.add(textArea, BorderLayout.CENTER);
+		c.add(button, BorderLayout.SOUTH);
+		
+		//	Add behavior
+		//	ActionListener is an interface, not a class
+		button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				textArea.append("Hello\n");
+			}
+			
+		});
+	}
+}
