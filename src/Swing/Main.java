38b7f113b11c8ca6d039e4e93fb758ebc7fffa5b
Chris Murray <chris.murray.az@gmail.com>
2016-06-19 13:12:48 -0700

    Tutorial 1 finished

---
+++ src/Swing/Main.java
@@ -0,0 +1,16 @@
+package Swing;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		JFrame frame = new JFrame("Hello World Swing!");
+		frame.setSize(500,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+
+}
